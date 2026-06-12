8db98c412d0e0137715c7fccc7df078f8e59aa7f
tamayuma02-stack <tamayuma02@gmail.com>
2026-06-12 12:24:06 +0900

    homepage

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from fastapi import FastAPI
 import random
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -16,3 +17,25 @@ def read_item(item_id: int, q: Optional[str] = None):
 def omikuji():
     omikuji_list = ["大吉", "中吉", "小吉", "吉", "半吉", "末吉", "末小吉", "凶", "小凶", "大凶"]
     return {"result": omikuji_list[random.randrange(10)]} # APIらしく辞書型に変更しています
+
+
+
+
+
+
+
+### コードいろいろ... ###
+
+@app.get("/index")
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>It's My homepage</title>
+        </head>
+        <body>
+            <h1>I'm Yuma!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
